1b3ec10eaa95527f936d34413d78df797b4b1200
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2024-08-31 00:07:47 -0700

    begin example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Serialize;
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+use thirtyfour::prelude::*;
+
+use url::Url;
+
+
+
+
+
+
+#[tokio::main]
+async fn main() -> WebDriverResult<()> {
+    let caps = DesiredCapabilities::chrome();
+    let driver = WebDriver::new("http://localhost:9515", caps).await?;
+
+    // Navigate to https://wikipedia.org.
+    driver.goto("https://wikipedia.org").await?;
+    let elem_form = driver.find(By::Id("search-form")).await?;
+
+    // Find element from element.
+    let elem_text = elem_form.find(By::Id("searchInput")).await?;
+
+    // Type in the search terms.
+    elem_text.send_keys("selenium").await?;
+
+    // Click the search button.
+    let elem_button = elem_form.find(By::Css("button[type='submit']")).await?;
+    elem_button.click().await?;
+
+    // Look for header to implicitly wait for the page to load.
+    driver.find(By::ClassName("firstHeading")).await?;
+    assert_eq!(driver.title().await?, "Selenium - Wikipedia");
+
+    // explicitly close the browser.
+    driver.quit().await?;
+
+    Ok(())
 }
