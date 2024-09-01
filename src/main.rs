3bd2df7d42aba7fb4ab783af9174df5a457d6276
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2024-08-31 22:33:14 -0700

    navigate to nasdaq page

--- src/main.rs
+++ src/main.rs
@@ -14,25 +14,24 @@ use url::Url;
 #[tokio::main]
 async fn main() -> WebDriverResult<()> {
     let caps = DesiredCapabilities::chrome();
-    let driver = WebDriver::new("http://localhost:9515", caps).await?;
+    let driver = WebDriver::new("http://localhost:52281", caps).await?;
 
-    // Navigate to https://wikipedia.org.
-    driver.goto("https://wikipedia.org").await?;
-    let elem_form = driver.find(By::Id("search-form")).await?;
+    // check date and navigate to website 
+    driver.goto("https://www.nasdaq.com/market-activity/earnings").await?;
 
-    // Find element from element.
-    let elem_text = elem_form.find(By::Id("searchInput")).await?;
 
-    // Type in the search terms.
-    elem_text.send_keys("selenium").await?;
 
-    // Click the search button.
-    let elem_button = elem_form.find(By::Css("button[type='submit']")).await?;
+
+    let elem_form = driver.find(By::ClassName("time-belt__list")).await?;
+
+
+    // Find date elements that fall within the weekdays of the current date range
+    let elem_button = elem_form.find(By::Css("button[class='time-belt__item']")).await?;
     elem_button.click().await?;
 
     // Look for header to implicitly wait for the page to load.
     driver.find(By::ClassName("firstHeading")).await?;
-    assert_eq!(driver.title().await?, "Selenium - Wikipedia");
+    assert_eq!(driver.title().await?, "Earnings Calendar | Nasdaq");
 
     // explicitly close the browser.
     driver.quit().await?;
